898b1726bd369a012b4079c66efff90e64139e1b
abduhhtm <160695516+abduhhtm@users.noreply.github.com>
2024-08-05 13:19:07 +0700

    latihan it js4

---
+++ latihanjs4/latihanjs4.js
@@ -0,0 +1,51 @@
+var nilai = 80
+var kehadiran = 20
+var totalpertemuan = 25
+var presentasekehadiran = kehadiran/totalpertemuan*100
+
+
+if (nilai > 85) {
+ console.log("katagori a");
+  if (presentasekehadiran >= 75) {
+    console.log("lulus");
+  } else{
+    console.log("tidak lulus");
+  }
+
+} else if (nilai >70 && nilai <=85) {
+    console.log("katagori b");
+    if (presentasekehadiran >75) {
+        console.log("lulus");
+    }else {
+        console.log("tidak lulus");
+    }
+
+}  else if (nilai >=50 && nilai <=70) {
+    console.log("katagori c");
+    if (presentasekehadiran >75) {
+        console.log("lulus");
+    }else {
+        console.log("tidak lulus");
+    }
+
+} else if (nilai <50) {
+    console.log("tidak lulus");
+    if (presentasekehadiran >75) {
+        console.log("lulus");
+    }
+}
+
+// tugas gambar 2
+
+var pendaftaran = false;
+var pembayaran = false;
+
+if (pendaftaran === true) {
+  if (pembayaran === true) {
+    console.log("Dapat mengikuti Kursus");
+  } else {
+    console.log("Tidak dapat mengikuti Kursus");
+  }
+} else {
+  console.log("Tidak dapat mengikuti Kursus");
+}
